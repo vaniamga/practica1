879eb7608d5b30ca2650ea87648362416d879a3e
vaniamga <vaniagallardo578@gmail.com>
2023-07-28 13:53:02 -0600

    api

---
+++ vue-project/src/App.vue
@@ -0,0 +1,8 @@
+
+fetch('https://raw.githubusercontent.com/vaniamga/api/main/json/personas.json')
+  .then((res) => res.json())
+  .then((data) => posts.value = data)
+  .catch((e) => console.log (e))
+  .finally(() => {
+
+  }
---
+++ vue-project/src/components/card.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+    title: String,
+    author: String,
+    year: Number,
+    body: {
+        type : String,
+        default: "sin descripcion",
+    }
+
+})
+const emit = defineEmits(['añadirFavorito'])
+</script>
+
+<template>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                    <h5 class="card-title">{{title}} - {{ year }} - {{ author }} </h5>
+                        <p class="card-text">{{body}}</p>
+                        <div class="d-grid">
+                            <button class="btn btn-primary" type="button" @click="emit('añadirFavorito', id)">Añadir a favoritos</button>
+                        </div>
+                    </div>
+                </div>    
+        </div>
+</template>
